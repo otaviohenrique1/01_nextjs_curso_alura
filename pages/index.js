8340af55e7ebd6d9901d51c099b158a0850889c0
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-10-10 13:46:16 -0300

    commit 4

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+/* import Link from "../src/components/Link";
 import Title from "../src/components/Title";
+import Footer from "../src/components/patterns/Footer";
 
 export default function HomePage() {
   return (
     <div>
       <Title as="h1">Ola mundo</Title>
       <Link href="/faq" passHref>Ir para a pagina do FAQ</Link>
+      <Footer />
     </div>
   );
-}
+} */
