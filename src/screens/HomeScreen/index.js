7a88abeac3ee077e772da2448570a4d92a100f9e
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-10-10 18:59:27 -0300

    commit 5

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,9 +2,10 @@ import Footer from '../../components/patterns/Footer';
 import Link from '../../components/Link';
 import { theme } from '../../theme/theme';
 import { Image, Box, Text, Icon, Input, Button } from '../../theme/components';
+import PageTitle from '../../components/PageTitle';
 
-const LOGO_ALURA_URL = 'http://placehold.it/94x44';
-const SIDE_IMAGE_URL = 'http://placehold.it/2878x1640';
+const LOGO_ALURA_URL = '/assets/icons/alura-logo.svg';
+const SIDE_IMAGE_URL = '/assets/images/alura-cases.png';
 
 function SideImage() {
   return (
@@ -96,6 +97,7 @@ function SideImage() {
 export default function HomeScreen() {
   return (
     <Box>
+      <PageTitle>Home - Alura Cases Campanha</PageTitle>
       <Box
         as="main"
         styleSheet={{
